a5be1de19d734ca261b2c5a091e9c1e50210ca4e
Joshi004 <thinkjoshi@gmail.com>
2019-09-27 09:53:48 -0700

    add dynamic form

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 // import  LoginComponent   from './LogIn/login';
 import LoginContainer from './login/LoginContainer'
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+import DynamicFormComponent from './DynamicForm/dform'
 
 function App() {
   return (
@@ -10,6 +11,18 @@ function App() {
       <MuiThemeProvider>
         <LoginContainer></LoginContainer>
       </MuiThemeProvider>
+      <DynamicFormComponent
+       title="New Survey"
+       model = {[
+           {key:"0",label:"Name",props:{required:true}},
+           {key:"1",label:"Age",type:"number"},
+           {key:"2",label:"Rating",type:"number",props:{min:0,max:5}},
+           {key:"3",label:"Qualification"},
+           {key:"4",label:"What is your age",type:"number",props:{min:0,max:100}}
+           ]}
+           onSubmit = {(model)=> { this.onSubmit(model) }}
+      >
+      </DynamicFormComponent>
       
     </div>
   );  
